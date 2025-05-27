efc5114469946636d02289100ed4a7858b08fea0
bestlife.nakama <bestlife.nakama@google.com>
2025-05-27 12:30:17 +0900

    feat: メニューとタグのモデルに多対多のリレーションを設定

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Menu extends Model
 {
-    //
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
