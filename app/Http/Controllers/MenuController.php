bc0a75dbdf89e46163e1631330cde82d137d3f8c
bestlife.nakama <bestlife.nakama@google.com>
2025-05-30 11:43:58 +0900

    feat: メニュー一覧のキーワードと並び替え条件にバリデーションを設定

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,25 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MenuIndexRequest;
 use App\Models\Menu;
-use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    private const SORT_LIST = [
+    public const SORT_LIST = [
         '作成日の新しい順' => ['column' => 'created_at', 'direction' => 'desc'],
         '作成日の古い順' => ['column' => 'created_at', 'direction' => 'asc'],
         '更新日の新しい順' => ['column' => 'updated_at', 'direction' => 'desc'],
         '更新日の古い順' => ['column' => 'updated_at', 'direction' => 'asc'],
     ];
 
-    public function index(Request $request) {
+    public function index(MenuIndexRequest $request) {
 
         $sort_list = self::SORT_LIST;
         
         $query = Menu::query();
 
-        $keyword = $request->query('keyword');
+        $validated = $request->validated();
+
+        $keyword = $validated['keyword'] ?? null;
         if ($keyword) {
             $query->where('title', 'like', "%{$keyword}%")
                     ->orWhere('content', 'like', "%{$keyword}%")
@@ -35,7 +37,7 @@ class MenuController extends Controller
 
         $query->with('user')->with('tags');
 
-        $sort_type = $request->query('sort_type');
+        $sort_type = $validated['sort_type'] ?? null;
         if ($sort_type) {
             $column = self::SORT_LIST[$sort_type]['column'];
             $direction = self::SORT_LIST[$sort_type]['direction'];
---
+++ app/Http/Requests/MenuIndexRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Controllers\MenuController;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class MenuIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'keyword' => ['nullable', 'string',  'max:255'],
+            'sort_type' => ['nullable', 'string', Rule::in(array_keys(MenuController::SORT_LIST))],
+        ];
+    }
+
+    public function validationData()
+    {
+        return $this->query();
+    }
+}
