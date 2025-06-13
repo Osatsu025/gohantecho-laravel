5e59e54e4dab08d318d2fee06f2f2bda2b6872ed
bestlife.nakama <bestlife.nakama@google.com>
2025-06-13 10:39:11 +0900

    test: メニューのCRUDに関するテスト

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,8 +11,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Menu extends Model
 {
-    use SoftDeletes;
-    
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'title',
         'content',
