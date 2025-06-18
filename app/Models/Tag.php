119467b3b43320896c2ca82ea2e8dafe600c02fc
bestlife.nakama <bestlife.nakama@google.com>
2025-06-18 10:53:45 +0900

    feat: タグにソフトデリートを追加

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
     ];
+    
     public function menus(): BelongsToMany
     {
         return $this->belongsToMany(Menu::class);
