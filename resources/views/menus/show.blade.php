c63b76af50774ec805df0c06c9cc590f211c8ce4
bestlife.nakama <bestlife.nakama@google.com>
2025-07-09 10:53:19 +0900

    feat: メニュー詳細にお気に入り追加/削除機能を実装

--- resources/views/menus/show.blade.php
+++ resources/views/menus/show.blade.php
@@ -37,7 +37,7 @@
               </a>
               @endcan
               @can('delete', $menu)
-              <button class="btn btn-error" onclick="delete_modal.showModal()">
+              <button class="btn btn-error mr-6" onclick="delete_modal.showModal()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
                 </svg>
@@ -59,6 +59,20 @@
                 </div>
               </dialog>
               @endcan
+              <form method="POST" action="{{ query_route('menus.favorite', ['menu' => $menu]) }}">
+                @csrf
+                <button type="submit">
+                  @if($menu->favoritedUsers->contains(Auth::user()))
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-heart-fill fill-pink-500" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                  </svg>
+                  @else
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                  </svg>
+                  @endif
+                </button>
+              </form>
             </div>
             <p class="mb-4">{!! nl2br(e($menu->content)) !!}</p>
             <div>
