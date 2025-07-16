c5a326c79ab766284468bfe401b2c35f9a3c97c6
bestlife.nakama <bestlife.nakama@google.com>
2025-07-16 10:48:36 +0900

    feat: メモをソフトデリートに

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Memo extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'menu_id',
         'content',
